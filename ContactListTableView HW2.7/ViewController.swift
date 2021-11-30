33fb0694cfa49dddcc98a808d21282849d425ffe
Sergey-Yurtaev <engineerk2@mail.ru>
2021-11-30 10:28:41 +0300

    Initial Commit

---
+++ ContactListTableView HW2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListTableView HW2.7
+//
+//  Created by Sergey Yurtaev on 30.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
